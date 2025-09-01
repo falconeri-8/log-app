ba3bb36fe1137ee761df91ee1fa09a79c3070f51
falconeri <hananeight@gmail.com>
2025-09-02 01:36:08 +0500

    Revert "Merge remote-tracking branch 'origin/vercel'"
    This reverts commit 4d7abb4215241a78e15eb96cc2d25ae853779566, reversing
    changes made to c2df78e1bece2896e98ca6785a50faf68a0d0537.

--- host/routes/profile.js
+++ host/routes/profile.js
@@ -79,7 +79,6 @@ router.post('/filtered', auth, async (req, res) => {
             $or: [
                 {name: {$regex: search, $options: 'i'}},
                 {contact: {$regex: search, $options: 'i'}},
-                {author: {$regex: req.author.id, $options: 'i'}},
             ]
         } : {author: req.author.id};
         
